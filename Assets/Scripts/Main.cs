bc44f95d0547aa41e2a7189a6873cd503e99a874
ricardo limo <ricardoclimo@live.com>
2018-04-21 14:59:58 -0700

    Adding unity project structure

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	public championsInfo test = new championsInfo();
+	public Logger logger;
+	// Use this for initialization
+	void Start () {
+		test.readJSON();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
